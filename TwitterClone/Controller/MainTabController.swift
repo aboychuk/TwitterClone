753e3919a7f312d7a570929b1bffa0e82201d607
Andrew Boychuk <andrewboychuk@gmail.com>
2020-10-05 13:49:56 +0200

    ViewController deleted, TabBarController added and set as rootViewController

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  TwitterClone
 //
 //  Created by Andrii Boichuk on 05/10/2020.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
     }
-
-
 }
-
